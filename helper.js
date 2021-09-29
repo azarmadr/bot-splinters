62dde3442f099d22c288984b0dfe119a61f9cd1a
azarmadr <azarshaik454@gmail.com>
2021-09-29 14:39:13 +0530

    repairing gold deck color selection

--- helper.js
+++ helper.js
@@ -1,25 +1,15 @@
 const cards = require("./data/cards.json");
 
-const makeCardId = (id) => id;
-
-const color = (id) => {
-    const card = cards.find(o => o.id === id);
-    const color = card && card.color ? card.color : '';
-    return color;
-}
-
-// const teamIdsArray = [167, 192, 160, 161, 163, 196, '', 'fire'];
-
+const color=(c)=>{return cards[c.id-1]?cards[c.id-1].color:''}
 const validDecks = ['Red', 'Blue', 'White', 'Black', 'Green']
 const colorToDeck = { 'Red': 'Fire', 'Blue': 'Water', 'White': 'Life', 'Black': 'Death', 'Green': 'Earth' }
 
-const deckValidColor = (accumulator, currentValue) => validDecks.includes(color(currentValue.id)) ? colorToDeck[color(currentValue.id)] : accumulator;
+const deckValidColor=(accumulator,currentValue)=>validDecks.includes(color(currentValue))?colorToDeck[color(currentValue)]:accumulator;
 
-//console.log(teamIdsArray.reduce(deckValidColor, ''));
+const teamActualSplinterToPlay=(teamIdsArray)=>teamIdsArray.reduce(deckValidColor,'Fire')
 
-const teamActualSplinterToPlay = (teamIdsArray) => teamIdsArray.reduce(deckValidColor, '')
+//const teamIdsArray = [{"id":62,"level":1,"name":"Living Lava"},{"id":61,"level":1,"name":"Kobold Miner"}]; console.log(teamActualSplinterToPlay(teamIdsArray));
+//console.log(color({"id":224,"level":1,"name":"Drake of Arnak"}))
 
-
-module.exports.makeCardId = makeCardId;
 module.exports.color = color;
 module.exports.teamActualSplinterToPlay = teamActualSplinterToPlay;
