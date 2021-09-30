84d6aae31476de45293091e674d790626a84c72c
azarmadr <azarshaik454@gmail.com>
2021-09-30 14:12:14 +0530

    script

--- t.js
+++ t.js
@@ -11,9 +11,9 @@ const winningTeams = require('./battle').winningTeams;
 //const b=battles.battlesList(player,fn); Promise.resolve(b).then(x=>{
   //Promise.resolve(require('./user').getPlayerCards(player)).then(()=>{
     console.log(x.length);
-    console.log(score(x).Standard[12].team.playable[0]);
+    //console.log(score(x).Standard[12].team.playable[0]);
     //console.log(Object.values(winningTeams(x,fn))[0][0].w[0]);
-    //score_opt.filterOutByMana=true;score_opt.filterOutLowWR=true;score_opt.StandardOnly=true; const s = score(x,score_opt,'foscor'); console.log(s[12].team.playable[0])
+    score_opt.filterOutByMana=true;score_opt.filterOutLowWR=true;score_opt.StandardOnly=true; const s = score(x,score_opt,'foscor'); console.log(s[12].team.playable[0])
     //score_opt.filterOutByMana=true;delete score_opt.StandardOnly;delete score_opt.filterOutLowWR; const s1 = score(x,score_opt,'sfo_score'); console.log(s1[12].team.playable[0])
   //})
 //})
