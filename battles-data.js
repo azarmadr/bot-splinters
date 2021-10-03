49562e7333c8638559d3e23142f2505931fb1fd9
azarmadr <azarshaik454@gmail.com>
2021-10-03 16:35:48 +0530

    moving to array data

--- battles-data.js
+++ battles-data.js
@@ -1,112 +1,54 @@
-const fetch = require("node-fetch");
-const fs = require('fs');
-
+const {readFile,writeFile} = require('jsonfile');
+const {arrEquals,arrCmp} = require('./helper');
+const log=(...m)=>console.log('battles-data: ',...m);
 var pc = 0;
-const distinct = (value, index, self) => {
-  return self.indexOf(value) === index;
-}
-const cleanTeam = (team) => {return {summoner: team.summoner,monsters: team.monsters}}
-const teamsAsKey = (teams) => {
-  const a = JSON.stringify(cleanTeam(teams[0]));
-  const b = teams[1]?JSON.stringify(cleanTeam(teams[1])):'';
-  if(a===b&&teams[0].verdict=='d'){ teams.pop(); return a; }
-  return (a>b)?(a+b):(b+a);
-}
-
-async function getBattleHistory(player = '', data = {}) {
-  const battleHistory = await fetch('https://game-api.splinterlands.io/battle/history?player=' + player)
-    .then((response) => {
-      if (!response.ok) { throw new Error('Network response was not ok '+player); }
-      return response;
-    })
-    .then((battleHistory) => { return battleHistory.json(); })
+async function getBattleHistory(player = '') {
+  const battleHistory = await require('async-get-json')(`https://game-api.splinterlands.io/battle/history?player=${player}`)
+    .then(b=>b.battles)
     .catch((error) => {
-      console.log('There has been a problem with your fetch operation:', error);
+      log('There has been a problem with your fetch operation:', error);
       return [];
     });
   require('readline').clearLine(process.stdout,0)
   require('readline').cursorTo(process.stdout,0);
   process.stdout.write(`${pc+++' '+player}`);
-  return battleHistory.battles;
+  return battleHistory;
 }
 
-const extractGeneralInfo = (x) => {
-  return {
-    created_date:  x.created_date ? x.created_date :  '',
-    mana_cap:      x.mana_cap     ? x.mana_cap :      '',
-    ruleset:       x.ruleset      ? x.ruleset :       '',
-  }
-}
-
-const extractMonster = (team) => {
-  const extractCard = (card) => {
-    return {id:card.card_detail_id,level:card.level}
-  }
-  return {
-    summoner: extractCard(team.summoner),
-    monsters: [...team.monsters.map(extractCard)],
-  }
-}
-const teamFromBattles = (battles) => battles.map(
-  battle => {
-    const {type,winner,team1,team2,seed} = JSON.parse(battle.details);
-    if (Date.parse(battle.created_date)>1631856895886 && type != 'Surrender') {
-      const teams = [];
-      const info = extractGeneralInfo(battle)
-      const t1mon = extractMonster(team1)
-      const t2mon = extractMonster(team2)
-      teams.push({
-        ...t1mon,
-        verdict: (winner && winner == battle.player_1)?'w':(winner == 'DRAW')? 'd' :'l',
-      })
-      if(JSON.stringify(t1mon)!==JSON.stringify(t2mon)){
-        teams.push({
-          ...t2mon,
-          verdict: (winner && winner == battle.player_2)?'w':(winner == 'DRAW')? 'd' :'l',
-        })}
-      return {
-        ...info,
-        battle_id: seed,
-        teams: teams,
-      }
+const __medusa=(m)=>(m.card_detail_id==194&&m.level<3)?17:m.card_detail_id;
+const xtractTeam=(t)=>[...[t.summoner,...t.monsters].map(m=>[__medusa(m),m.level])]
+const genBattleList=(battles)=>battles.map(
+  ({ruleset,mana_cap,created_date,details})=>{
+    const {type,winner,team1,team2,seed} = JSON.parse(details);
+    if (Date.parse(created_date)>1631856895886 && type != 'Surrender') {
+      const teams = [team1,team2].map(xtractTeam);
+      if(arrEquals(...teams)){return undefined}
+      const won = new Proxy({winner},{get: (t,p,r)=>{//target,prop,reciever
+        if(t.winner=='d'){return 'd'}
+        return t.winner==p?'w':'l';
+      }});
+      [team1,team2].forEach(({player},i)=>teams[i].push(won[player]));
+      return{date:created_date,mana:mana_cap,rule:ruleset,id:seed,teams}
     }
-  })
-var battlesList = [];
-let promises = [];
+  }
+)
+
 const battles = (player,fn='') => getBattleHistory(player)
-  .then(u => u.map(x => {
-    return [x.player_1, x.player_2]
-  }).flat().filter(distinct))
-  .then(ul => ul.map(user => {
-    promises.push(
-      getBattleHistory(user)
-      .then(teamFromBattles)
-      .then(x => battlesList = [...battlesList, ...x])
-    )
-  }))
-  .then(() => { return Promise.all(promises) })
-  .then(() => { return new Promise((res,rej) => {
-    console.log(battlesList.length,' battles this session')
-    fs.readFile(`./data/battle_data${fn}.json`, 'utf8', (err, data) => {
-      if (err) {
-        console.log(`Error reading file from disk: ${err}`);
-      } else {
-        battlesList = data ? [...JSON.parse(data),...battlesList] : battlesList;
-      }
-      console.log('battles',battlesList.length-2550)
-      battlesList = battlesList.filter(x => x != undefined);
-      battlesList.forEach(b=>b.teams.forEach(t=>{
-        const m = t.monsters.find(m=>m.id===194 && m.level<3);m?m.id=17:'';
-      }))
-      battlesList = [...new Map(battlesList.map(item => [teamsAsKey(item.teams), item])).values()];
-      console.log('battles',battlesList.length)
-      fs.writeFile(`data/battle_data${fn}.json`, JSON.stringify(battlesList), function (err) {
-        if (err) {
-          console.log(err)//;rej(err)
-        }
-      });
-      res(battlesList)
-    });
-  })})
+  .then(u => u.reduce((d,c)=>[...new Set([...d,c.player_1,c.player_2])],[]))
+  .then(ul=>ul.map(u=>getBattleHistory(u).then(genBattleList)))
+  .then(ul=>Promise.all(ul).then(x=>x.flat().filter(x=>x)))
+  .then(b=>{ return new Promise((res,rej) =>
+    readFile(`./data/battle_data_n${fn}.json`,(e,d)=>{
+      let battlesList = b,__c=b.length;
+      log("\n",__c,' battles this session');
+      if(e){log('Error reading file: ',e)}
+      else{ d && (battlesList = [...d,...battlesList])}
+      log('battles',__c=battlesList.length-__c);
+      battlesList = [...new Map(battlesList.map(item => [item.teams.map(t=>t.slice(0,-1)).sort(arrCmp)+'', item])).values()];
+      log('battles',battlesList.length-__c,' added')
+      writeFile(`data/battle_data_n${fn}.json`, battlesList).catch(e=>log(e))
+      res(battlesList);
+      })
+  )})
 
 module.exports.battlesList = battles;
