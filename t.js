c05450201f7c959496bd4462ce1aa34f68a0553f
azarmadr <azarshaik454@gmail.com>
2021-10-07 18:54:44 +0530

    organized properly

--- t.js
+++ t.js
@@ -1,19 +1,25 @@
 require('dotenv').config();
+const log  = (...m)=>console.log('Tests:',...m);
 const args = require('minimist')(process.argv.slice(2));
-const player = args.name||process.env.ACCOUNT;
-const fn = args.fn||'';
-const score = require('./score').scores;
-const battles = args.seq?require('./battles-data-seq'):require('./battles-data');
-const cards = require('./data/cards.json');
+
+const {teamScores,playableTeams} = require('./score');
+const cards                      = require('./data/cards.json');
+const oldBattles                 = require('./data/battle_data.json');
+const {winningTeams}             = require('./winningTeams');
+
+const player  = args.name         ||process.env.ACCOUNT;
+const fn      = args.fn           ||'';
+const battles = args.seq          ?require('./battles-data-seq'):require('./battles-data');
+const b       = args.freshBattles ?battles.battlesList(player,fn):oldBattles;
+
 const score_opt = {};
-const x=require('./data/battle_data.json');
-const winningTeams = require('./winningTeams').winningTeams;
-const b=battles.battlesList(player,fn); Promise.resolve(b).then(x=>{
+Promise.resolve(b).then(x=>{
   //Promise.resolve(require('./user').getPlayerCards(player)).then(()=>{
-    console.log(x.length);
-    //console.log(score(x).Standard[12].team.playable[0]);
-    console.log(Object.values(winningTeams(x,player,fn))[0][0].w[0]);
-    score_opt.filterOutByMana=true;score_opt.filterOutLowWR=true;score_opt.StandardOnly=true; const s = score(x,player,score_opt,'foscor'); console.log(s[12].team.playable[0])
-    //score_opt.filterOutByMana=true;delete score_opt.StandardOnly;delete score_opt.filterOutLowWR; const s1 = score(x,score_opt,'sfo_score'); console.log(s1[12].team.playable[0])
+    log(x.length);
+    //log(score(x,{},fn).entries().next());
+    log(playableTeams(teamScores(x,{}),player,27,'Standard',{},'test').entries().next());
+    //log(Object.values(winningTeams(x,player,fn))[0][0].w[0]);
+    //score_opt.filterOutByMana=true;score_opt.filterOutLowWR=true;score_opt.StandardOnly=true; const s = score(x,player,score_opt,'foscor'); log(s[12].team.playable[0])
+    //score_opt.filterOutByMana=true;delete score_opt.StandardOnly;delete score_opt.filterOutLowWR; const s1 = score(x,score_opt,'sfo_score'); log(s1[12].team.playable[0])
   //})
 })
