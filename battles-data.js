2bf9d9630e84fe49539544c599761fb2c6ba7327
azarmadr <azarshaik454@gmail.com>
2021-11-03 22:10:45 +0530

    cleaner battles data with usable functions

--- battles-data.js
+++ battles-data.js
@@ -5,7 +5,7 @@ var pc = 0;
 const RULES_ON_CARDS = 'Broken Arrows,Even Stevens,Keep Your Distance,Little League,Lost Legendaries,Lost Magic,Odd Ones Out,Rise of the Commons,Taking Sides,Up Close & Personal'
 async function getBattleHistory(player = '') {
   const battleHistory = await require('async-get-json')(`https://game-api.splinterlands.io/battle/history?player=${player}`)
-    .then(b=>b.battles)
+    .then(b=>b.battles.filter(b=>!b.details.includes('"type":"Surrender"')))
     .catch((error) => {
       log('There has been a problem with your fetch operation:', error);
       return [];
@@ -20,8 +20,8 @@ const __medusa=(m)=>(m.card_detail_id==194&&m.level<3)?17:m.card_detail_id;
 const xtractTeam=(t)=>[...[t.summoner,...t.monsters].map(m=>[__medusa(m),m.level])]
 const genBattleList=(battles)=>battles.map(
   ({ruleset,mana_cap,created_date,details})=>{
-    const {type,winner,team1,team2,seed} = JSON.parse(details);
-    if (Date.parse(created_date)>1631856895886 && type != 'Surrender') {
+    const {winner,team1,team2} = JSON.parse(details);
+    if (Date.parse(created_date)>1631856895886) {
       const teams = [team1,team2].map(xtractTeam);
       if(arrEquals(...teams)){return undefined}
       const won = new Proxy({winner},{get: (t,p,r)=>{//target,prop,reciever
@@ -29,20 +29,27 @@ const genBattleList=(battles)=>battles.map(
         return t.winner==p?'w':'l';
       }});
       [team1,team2].forEach(({player},i)=>teams[i].unshift(won[player]));
+      teams.sort();
       return{mana:mana_cap,rule:(RULES_ON_CARDS.includes(ruleset)?'Standard':ruleset),teams}
     }
   }
 ).filter(x=>x)
 
-const battles = (player,fn='') => getBattleHistory(player)
-  .then(bh => bh.reduce((d,c)=>[...new Set([...d,c.player_1,c.player_2])],[]))
-  .then(ul=>Promise.resolve(chunk(ul,27).reduce(
-    (memo,ul_chunk)=>memo.then(bl=>
-      Promise.all(ul_chunk.map(u=>getBattleHistory(u).then(genBattleList)))
-      .then(gbl=>[...bl,...gbl.flat()])
-    ),Promise.resolve([])
-  )))
-  .then(bl=>{ return new Promise((res,rej) =>
+/** Generates battles list from an array of users. First it is chunked to an array of smaller array, and then gets battlesList for each chunk, concats them all to a final array.
+ * @param {Array String} ul array of users
+ * @param {Number} cl chunk length
+ * @param {String} fn additional filename postfix to be added to `./data/battle_data`
+ * @returns {Array battle} array of battles and saves the same
+ */
+const userListToBattlesList=(ul,cl=27)=>Promise.resolve(chunk(ul,cl).reduce(
+  (memo,ul_chunk)=>memo.then(bl=>
+    Promise.all(ul_chunk.map(u=>getBattleHistory(u).then(genBattleList)))
+    .then(gbl=>[...bl,...gbl.flat()])
+  ),Promise.resolve([])
+))
+
+const saveBattleList=(bl,fn='')=>{
+  return new Promise((res,rej) =>
     readFile(`./data/battle_data${fn}.json`,(e,d)=>{
       let battlesList = bl,__c=bl.length;
       console.log();
@@ -51,13 +58,25 @@ const battles = (player,fn='') => getBattleHistory(player)
       else{ d && (battlesList = [...d,...battlesList])}
       log('battles',__c=battlesList.length-__c);
       battlesList = [...new Map(battlesList.map(item =>
-        [item.rule+item.mana+item.teams.sort(), item]
-      )).values()];
+        [JSON.stringify(item),item]
+      )).values()]
       log('battles',battlesList.length-__c,' added')
       writeFile(`data/battle_data${fn}.json`, battlesList).catch(e=>log(e))
       res(battlesList);
-      })
-  )})
+    })
+  )}
 
-module.exports.battlesList = battles;
-//Promise.resolve(battles('azarmadr3','-test')).then(b=>log(b[0],b.at(-1),b.filter(a=>a.teams.length<2).length))
+/** Generates user list from a given player battle history, and passes it on to userListToBattlesList function to create aggregate battles list, and saves it to a file
+ * @param {String} player Splinterlands account name of a player
+ * @param {String} fn additional filename postfix to be added to `./data/battle_data`
+ * @returns {Array battle} array of battles
+ */
+const battles = (player,fn) => getBattleHistory(player)
+  .then(bh => bh.reduce((d,c)=>[...new Set([...d,c.player_1,c.player_2])],[]))
+  .then(ul=>userListToBattlesList(ul,27))
+  .then(bl=>saveBattleList(bl,fn))
+
+module.exports = {
+  battlesList: battles, userListToBattlesList,saveBattleList
+}
+//Promise.resolve(battles('azarmadr','-test')).then(b=>log(b[0],b.at(-1)))
