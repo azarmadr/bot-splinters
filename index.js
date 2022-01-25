03d643e485ac42ea7cb8938bbd1250f8233b2315
azarmadr <azarshaik454@gmail.com>
2022-01-25 18:35:26 +0530

    code mods

--- index.js
+++ index.js
@@ -18,6 +18,25 @@ console.log = function () {
   logFile.write(util.format.apply(null,arguments).replace(/\033\[[0-9;]*m/g,"") + '\n');
 }
 
+const focusQueue = [];
+let queueStarted = false;
+function requestFocus(page) {
+  return new Promise(resolve => {
+    focusQueue.push(() => {
+      queueStarted = true;
+      page.bringToFront();
+      resolve(dequeue);
+    });
+    // do first dequeue if queue was empty
+    if (!queueStarted) dequeue();
+  });
+}
+
+function dequeue() {
+  const nextFocus = focusQueue.shift();
+  if (nextFocus) nextFocus(); else queueStarted = false;
+}
+
 let __goOn=1;
 const sleepingTime = 6e4 * (args.i ?? process.env.MINUTES_BATTLES_INTERVAL ?? 27);
 
@@ -68,10 +87,10 @@ async function createBrowser(headless) {
   });
   await browser.defaultBrowserContext().overridePermissions('https://splinterlands.com/',['notifications']);
   const page = await browser.newPage();
-  page.setDefaultNavigationTimeout(500000);
+  page.setDefaultNavigationTimeout(5e5);
   page.on('dialog', async dialog => { await dialog.accept(); });
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
-  await page.setViewport({ width: 1800, height: 1500, deviceScaleFactor: 1, });
+  await page.setViewport({ width: 1800, height: 1200, deviceScaleFactor: 1, });
   return browser;
 }
 const postBattle=user=>battle=>{
@@ -83,29 +102,12 @@ const postBattle=user=>battle=>{
   }else user.won<0?user.isRanked?user.l++:user.l_p++:user.isRanked?user.d++:user.d_p++;
   user.netWon+=user.won;
 }
-//async function teamSelection(teamToPlay,{page,inactive,ruleset,notifyUser}){ }
-async function startBotPlayMatch(page,user) {
-  const {mana_cap, ruleset, inactive, opponent_player,} = await SM.battle(user.isRanked?'Ranked':'Practice')
-  const myCards = await SM.cards(user.account).then(cards2Obj(user.account))
-    .then(c=>Object.fromEntries(Object.entries(c).filter(x=>!inactive.includes(_card.color(x)))))
-    .catch(e => log(e,'cards collection api didnt respond. Did you use username? avoid email!')??{});
-  const oppCards = await SM.cards(opponent_player).then(cards2Obj(opponent_player))
-    .then(c=>Object.fromEntries(Object.entries(c).filter(x=>!inactive.includes(_card.color(x)))))
-    .catch(e=>log(e,'Opp Cards Failed')??{})
-  table([{mana_cap, ruleset, inactive,quest:user.quest,[`${user.account} Deck`]:Object.keys(myCards).length,[`${opponent_player} Deck`]:Object.keys(oppCards).length}])
-  if(Object.keys(oppCards).length)table(__oppDeck=Object.entries(oppCards).map(([Id,Lvl])=>{
-    return{[_card.type(Id).slice(0,3)]:_card.name(Id),Id,Lvl,[_card.color(Id).slice(0,2)]:_card.abilities([Id,Lvl]).join()}})
-    .sort((a,b)=>('Mon'in a)-('Mon'in b)))
-  var battlesList =await getBattles(opponent_player).catch(e=>{log(e);return require('./data/battle_data.json')});
-  const teamToPlay = playableTeams(battlesList,
-    {mana_cap,ruleset,inactive,quest:user.quest,oppCards,myCards,sortByWinRate:user.isStarter||!user.isRanked})[0];
-  // team Selection await teamSelection(teamToPlay,{page,ruleset,inactive,notifyUser:!process.env.HEADLESS&&user.isRanked&&!user.isStarter});
-  const notifyUser=!process.env.HEADLESS&&user.isRanked&&!user.isStarter;
+async function teamSelection(teamToPlay,{page,inactive,ruleset,notifyUser}){
   const {team:[Summoner,...Monsters],...Stats} = teamToPlay;
   if(!ruleset.includes('Taking Sides')){
     const __medusa = Monsters.find(m=>m[0]==17);__medusa&&(__medusa[0]=194)
   }
-  table([...teamToPlay.team.map(([Id,Lvl])=>{return{[_card.type(Id)]:_card.name(Id),Id,Lvl}})]);
+  table([...teamToPlay.team.map(([Id,Lvl])=>({[_card.type(Id)]:_card.name(Id),Id,Lvl}))]);
   table({Stats});
   if(notifyUser)await page.evaluate(`var n=new Notification('Battle Ready');
       n.addEventListener('click',(e)=>{window.focus();e.target.close();},false);`);
@@ -125,7 +127,26 @@ async function startBotPlayMatch(page,user) {
   if(notifyUser)await sleep(Math.min(60,Math.abs(process.env.PAUSE_BEFORE_SUBMIT))*999);
   await retryFor(3,300,__goOn,async()=>page.click('.btn-green')).catch(log);
   log('Team submitted, Waiting for opponent');
-  // Eof TeamSelection
+}
+async function startBotPlayMatch(page,user) {
+  const {mana_cap, ruleset, inactive, opponent_player,} = await SM.battle(user.isRanked?'Ranked':'Practice')
+  const myCards = await SM.cards(user.account).then(cards2Obj(user.account))
+    .then(c=>Object.fromEntries(Object.entries(c).filter(x=>!inactive.includes(_card.color(x)))))
+    .catch(e => log(e,'cards collection api didnt respond. Did you use username? avoid email!')??{});
+  const oppCards = await SM.cards(opponent_player).then(cards2Obj(opponent_player))
+    .then(c=>Object.fromEntries(Object.entries(c).filter(x=>!inactive.includes(_card.color(x)))))
+    .catch(e=>log(e,'Opp Cards Failed')??{})
+  table([{mana_cap, ruleset, inactive,...user.quest,[`${user.account} Deck`]:Object.keys(myCards).length,[`${opponent_player} Deck`]:Object.keys(oppCards).length}])
+  if(Object.keys(oppCards).length)table(__oppDeck=Object.entries(oppCards).map(([Id,Lvl])=>{
+    return{[_card.type(Id).slice(0,3)]:_card.name(Id),Id,Lvl,[_card.color(Id).slice(0,2)]:_card.abilities([Id,Lvl]).join()}})
+    .sort((a,b)=>('Mon'in a)-('Mon'in b)))
+  var battlesList =await getBattles(opponent_player).catch(e=>{log(e);return require('./data/battle_data.json')});
+  const pt = playableTeams(battlesList,{mana_cap,ruleset,inactive,quest:user.quest,oppCards,myCards,sortByWinRate:user.isStarter||!user.isRanked});
+  table(pt.slice(0,5).map(({team,...s})=>({...team.map(c=>_card.name(c)+c[1]),...s})));
+  const teamToPlay = pt[0];
+  // team Selection
+  const rf = await requestFocus(page);
+  await teamSelection(teamToPlay,{page,ruleset,inactive,notifyUser:!process.env.HEADLESS&&user.isRanked&&!user.isStarter}); // Eof teamSelection
   await Promise.any([
     page.waitForSelector('#btnRumble', { timeout: 16e4 })
     .then(()=>page.evaluate(`startFightLoop();localStorage.setItem('sl:battle_speed', 6)`))
@@ -134,39 +155,38 @@ async function startBotPlayMatch(page,user) {
     page.waitForSelector('div.modal.fade.v2.battle-results.in',{timeout:(3e5)}),
   ]).then(()=>page.evaluate('SM.CurrentView.data').then(postBattle(user)))
     .catch(() => log('Wrapping up Battle'));
+  rf();
 }
 const calculateECR=({capture_rate, last_reward_time},{dec})=>
   Math.min(10000,(parseInt(capture_rate) || 10000) + (Date.now() - new Date(last_reward_time)) / 3000 * dec.ecr_regen_rate)/100;
-const preMatch=({Player,settings})=>{
-  const _return = {};
-  _return.dec = Player.balances.find(x=>x.token=='DEC')?.balance
+const preMatch=user=>({Player,settings})=>{
+  user.dec = Player.balances.find(x=>x.token=='DEC')?.balance
   const erc = calculateECR(Player,settings);
-  _return.erc = erc;
-  _return.rating = Player.rating;
-  _return.isStarter = !Player.starter_pack_purchase;
-  _return.cp = Player.collection_power;
-  _return.claimSeasonReward =
+  user.erc = erc;
+  user.rating = Player.rating;
+  user.isStarter = !Player.starter_pack_purchase;
+  user.cp = Player.collection_power;
+  user.claimSeasonReward =
     process.env.CLAIM_SEASON_REWARD && Player?.season_reward.reward_packs>0&&Player.starter_pack_purchase;
-  _return.isRanked = _return.isStarter||erc>75&&_return.rating<400||erc>(args.e??process.env.ERC_THRESHOLD)||
+  user.isRanked = user.isStarter||erc>81&&user.rating<400||erc>(args.e??process.env.ERC_THRESHOLD)||
     args.t-Date.now()>(99.81-erc)*3e5/settings.dec.ecr_regen_rate
 
   //if quest done claim reward
-  _return.claimQuestReward = [];
-  _return.quest = 0;
+  user.claimQuestReward = [];
+  user.quest = 0;
   if (Player.quest&&!Player.quest.claim_trx_id){
     var {name,completed_items,total_items}=Player.quest;
     const quest = settings.quests.find(x=>x.name==name)
     if(completed_items<total_items){
       if((Number(args.q)||3)*Math.random()<1&&process.env.QUEST_PRIORITY)
-        _return.quest = {type:quest.objective_type,color:quest.data.color,value:quest.data.value};
+        user.quest = {type:quest.objective_type,color:quest.data.color,value:quest.data.value};
     }
     if(completed_items>=total_items){
-      _return.claimQuestReward.push(Player.quest,quest);
-      _return.quest = 0;
+      user.claimQuestReward.push(Player.quest,quest);
+      user.quest = 0;
     }
   }
-  table([{Rating:Player.rating,ECRate:erc,t:(args.t-Date.now())/36e5,et:(100-erc)/12/settings.dec.ecr_regen_rate,...(completed_items&&{Quest:name,completed_items,total_items})}]);
-  return _return;
+  table([{Rating:Player.rating,ECRate:erc,t:(args.t-Date.now())/36e5,et:(99.81-erc)/12/settings.dec.ecr_regen_rate,...(completed_items&&{Quest:name,completed_items,total_items})}]);
 }
 const cards2Obj=acc=>cards=>Object.fromEntries(cards.map(card=>
   card.owned.filter(owned=>
@@ -204,8 +224,8 @@ const cards2Obj=acc=>cards=>Object.fromEntries(cards.map(card=>
       SM._(page);
       await SM.login(user.login || user.account,user.password)
       await page.evaluate('SM.ShowBattleHistory()'); await sleep(1729);
-      await page.evaluate('Object.assign({},{Player:SM.Player,settings:SM.settings})')
-        .then(preMatch).then(r=>Object.keys(r).forEach(k=>user[k]=r[k]))
+      await SM.cards(user.account)
+      await page.evaluate('Object.assign({},{Player:SM.Player,settings:SM.settings})').then(preMatch(user))
       if(process.env.CLAIM_REWARDS){
         if(user.claimSeasonReward)                         await page.evaluate('claim()');
         if(user.claimQuestReward?.filter(x=>x)?.length==2) await SM.questClaim(...user.claimQuestReward)
@@ -213,17 +233,17 @@ const cards2Obj=acc=>cards=>Object.fromEntries(cards.map(card=>
       if(!process.env.SKIP_PRACTICE||user.isRanked)await startBotPlayMatch(page,user).then(()=>console.log({
         '[]++++':'['+Array(9).fill(';;;;').join('')+'>','<~~~~~~~~':'|(xxxxx)'})).catch(async e=>{
         log(e,'failed to submit team, so waiting for user to input manually and close the session')
-        await sleep(163456);
+        await sleep(1.6e4);
         throw e;//can we continue here without throwing error
       })
       await page.evaluate('SM.Logout()');
     }
-    const tableList =['account','dec','erc','cp','rating','won','netWon','decWon','w','l','d','w_p','l_p','d_p'];
+    const tableList =['account','dec','erc','cp','rating','won','netWon','decWon','w','l','d',/*'w_p','l_p','d_p'*/];
     table(users.map(u=>Object.fromEntries(tableList.map(x=>[x,u[x]]))));
     if(!process.env.KEEP_BROWSER_OPEN)browser.close();
-    await battles.fromUsers(users.map(user=>user.account),{depth:1});
+    await battles.fromUsers(users.filter(u=>!process.env.SKIP_PRACTICE||u.isRanked).map(user=>user.account),{depth:1});
     log('Waiting for the next battle in',sleepingTime/1000/60,'minutes at',new Date(Date.now()+sleepingTime).toLocaleString());
-    log('--------------------------End of Session--------------------------------');
+    log('--------------------------End of Session--------------------------------\n\n');
     if(process.env.CLOSE_AFTER_ERC&&users.every(x=>x.isStarter||!x.isRanked)){ await browser.close(); break; }
     await sleep(sleepingTime);
   }
--- score.js
+++ score.js
@@ -51,42 +51,72 @@ function filterOutByMana(byMana=99){
 }
  */
 
-const _rarityScore=(id,level,x=6)=>(level==1&&_card.basic.includes(id))?1:(x**_card.rarity(id)*level)
-const scoreXer=(team,x=6)=>
-  _team.adpt(team).reduce((s,[id,level])=>_rarityScore(id,level,x)*(_card.mana(id)||1)+s,0)
+_score.rare=(id,level,x=6)=>(+level==1&&_card.basic.includes(id))?1:(x**Math.min(2,_card.rarity(id))*level)
+_score.Xer=(team,x=6)=>
+  _team.adpt(team).reduce((s,[id,level])=>_score.rare(id,level,x)*(_card.mana(id)||1)+s,0)
 const dotP=(x,y)=>Object.keys(x).reduce((sc,k)=>sc+x[k]*y[k],0)
+let defaultScores = {w:0,_w:0,l:0,_l:0,d:0,_d:0,count:0};
 
+const add2map=(m,k,v,p)=>(m[k]??={})[v]=p;
+const battle2nodeMatrix=b=>{
+  const nm = {};
+  for(let t in b)for(let [t1,r] of Object.entries(b[t])){
+    const p = r=='d'?1:2;
+    if(r=='l'||r=='d')add2map(nm,t1,t,p)
+    if(r=='w'||r=='d')add2map(nm,t,t1,p)
+  }
+  return nm;
+}
+const nm2inm=nm=>{
+  const inm = new Set();
+  for(let s in nm)for(let t of Object.keys(nm[s]))inm.add(t);
+  return inm;
+}
 const teamScores = (battles,{cardscores={},oppCards,myCards,res2Score={w:1,l:-1.1,d:-0.5},xer={r:1.2,s:6},mana_cap,inactive,_scoreAll}={}) => {
   const scores = new AKMap();
-  for(let t1 in battles)for(let t2 in battles[t1]){
-    let result = battles[t1][t2],teams = [t1,t2].map(x=>x.split(','));
-    if(teams.every(t=>!_team.isActive(t,inactive)))continue;
-    if(result=='l')[result,teams]=['w',teams.reverse()];
+  const setScores = (t,t1,p,xr=1)=>{
+    let teams = [t,t1].map(x=>x.split(',').map(Number));
+    if(teams.every(t=>!_team.isActive(t,inactive)))return;
     let [w,l] = teams.map((t,_x)=>
-      (t.some((c,x)=>x%2?0:(c in oppCards))?scoreXer(t,xer.r)/mana_cap:1)*(_team.mana(t)/mana_cap)**(_x?-1:1)/
+      (t.some((c,x)=>x%2?0:(c in oppCards))?_score.Xer(t,xer.r)/mana_cap:1)*(_team.mana(t)/mana_cap)**(_x?-1:1)/
       (_team.isActive(t,inactive)?1:3)
     );
-    let defaultScores = {w:0,_w:0,l:0,_l:0,d:0,_d:0,count:0};
     teams.forEach((t,i)=>{if(_scoreAll||t.every((c,x)=>x%2?1:c in myCards)){
       const cardScrs  = [scores.has(t)?scores.get(t):{...defaultScores},
         ..._arr.chunk2(t.slice(2)).map(([c],x,{length})=>
           (cardscores[c]??={p:{}}).p[x<length/2?x:x-length]??={...defaultScores})]
       cardScrs.forEach(cs=>{cs.count++;
-        if(result=='d'){cs._d++;cs.d+=i?w:l}
-        else{cs[i?'_w':'_l']++;cs[i?'w':'l']+=i?w:l;}
+        if(p==1){cs._d+=1/2;cs.d+=xr*(i?w:l)/2;}
+        else{cs[i?'_w':'_l']++;cs[i?'w':'l']+=xr*(i?w:l);}
       });
       scores.set(t,cardScrs[0]);
     }})
   }
+  const nm = battle2nodeMatrix(battles);
+  const _tail = {l:0};
+  do{
+    _tail.k=[];
+    _dbug.tt.hrc = {level:_tail.l,nodes:Object.keys(nm).length};
+    let inm = nm2inm(nm);
+    for(let t in nm)if(!inm.has(t)){
+      _tail.k.push(t);
+      for(let[t1,p] of Object.entries(nm[t]))setScores(t,t1,p,(_tail.l+1)/(_tail.l+3));
+      delete nm[t];
+      scores.delete(t.split(',').map(Number));
+    }
+    _tail.l++;
+  }while(_tail.k.length);
+  _dbug.tt.hrc.forEach((x,i,a)=>x['#DanglingNodes']=x.nodes-a[i+1]?.nodes);delete _dbug.tt.hrc;
+  for(let t in nm)for(let[t1,p]of Object.entries(nm[t]))setScores(t,t1,p);
   /* for research
   const xerDist = {};
-  scores.forEach((s,t)=>xerDist[scoreXer(t)]=Math.max(xerDist[scoreXer(t)]||0,s.count))
+  scores.forEach((s,t)=>xerDist[_score.Xer(t)]=Math.max(xerDist[_score.Xer(t)]||0,s.count))
   try{var xer = readFileSync('./data/xer.json')}catch{xer={}}
   xer[mana_cap]=xerDist;writeFileSync('./data/xer.json',xer);
   // for research*/
-  scores.forEach((s,t)=>s.score=scoreXer(_arr.chunk2(t),xer.s)*dotP(res2Score,s)/mana_cap**3);
+  scores.forEach((s,t)=>s.score=_score.Xer(t,xer.s)*dotP(res2Score,s)/mana_cap**3);
   Object.entries(cardscores).forEach(([c,cs])=>{
-    Object.values(cs.p).forEach(s=>s.score=_rarityScore(c,myCards[c])*dotP(res2Score,s));
+    Object.values(cs.p).forEach(s=>s.score=_score.rare(c,myCards[c])*dotP(res2Score,s));
     cs.score=Object.values(cs.p).reduce((tt,s)=>tt+s.w,0)
     cs.pos = Object.entries(cs.p).reduce((p,[i,s])=>cs[p]?.w>s.w?p:i,'-1')
   })
@@ -94,8 +124,8 @@ const teamScores = (battles,{cardscores={},oppCards,myCards,res2Score={w:1,l:-1.
 }
 
 const co=t=>'Gray'+_team.color(t).replace(/dGray/,'dRedWhiteBlueBlackGreen')
-const teamWithBetterCards=(betterCards,mycards,{mana_cap,ruleset,sortByWinRate})=>({team,...stats},idx)=>{
-  if(!sortByWinRate&&idx<3){
+_score.wBetterCards=(betterCards,mycards,{mana_cap,wBetterCards,ruleset,sortByWinRate})=>({team,...stats},idx)=>{
+  if(!wBetterCards&&!sortByWinRate&&idx<3){
     const bc=c=>{
       const  bc = betterCards[c[0]]?.find(x=>co(team).includes(x.color)&&!team.some(y=>y[0]==x.id));
       return bc?(log({[`-${_card.name(c)}`]:'+'+_card.name(bc.id),'@Team':idx})??[bc.id,bc.level]):c
@@ -129,7 +159,7 @@ const teamWithBetterCards=(betterCards,mycards,{mana_cap,ruleset,sortByWinRate})
   }
   return {team,...stats}
 }
-const cardPassRules=rule=>c=>{
+_score.cardRules=rule=>c=>{
   switch(rule){
     case'Lost Magic':return    _card.magic(c)==0;case'Up Close & Personal':return   _card.attack(c)>0
     case'Broken Arrows':return _card.ranged(c)==0;case'Keep Your Distance':return   _card.attack(c)==0
@@ -139,13 +169,13 @@ const cardPassRules=rule=>c=>{
     default:return true;
   }
 }
-const filterTeamByRules=(team,rule)=>{
+_score.filterTeamByRules=(team,rule)=>{
   if('Little League,Lost Legendaries,Rise of the Commons'.includes(rule))
-    return team.every(cardPassRules(rule));
+    return team.every(_score.cardRules(rule));
   else if('Taking Sides'===rule)
-    return (team.reduce((a,c)=>c[0]==19?a+1:a,0)<2)&&team.slice(1).every(cardPassRules(rule));
+    return (team.reduce((a,c)=>c[0]==19?a+1:a,0)<2)&&team.slice(1).every(_score.cardRules(rule));
   else if(_team.rules.secondary.includes(rule))
-    return team.slice(1).every(cardPassRules(rule));
+    return team.slice(1).every(_score.cardRules(rule));
   else return true
 }
 const filterAbilities=(ruleset,c)=>ablt=>{for(rule of ruleset.split(','))switch(rule){
@@ -163,7 +193,7 @@ const filterAbilities=(ruleset,c)=>ablt=>{for(rule of ruleset.split(','))switch(
   case'Holy Protection':return!ablt.match(  /Divine Shield/)
   case'Spreading Fury':return!ablt.match(   /Enrage/)
 }return true}
-const betterCards =(myCards,rule)=> Object.fromEntries(
+_score.bCards =(myCards,rule)=> Object.fromEntries(
   myCards.filter(c=>_card.type(c)=='Monster').map((c,_,mycards)=>{
     const color = _card.color(c);
     const allStats = ['abilities','mana','speed','ranged','magic','attack','armor','health'].filter(s=>
@@ -180,7 +210,7 @@ const betterCards =(myCards,rule)=> Object.fromEntries(
         && _arr.eqSet(...[c,oc].map(x=>_card.abilities(x).filter(filterAbilities(rule,x))));
     const better = mycards.filter(oc=>
       c[0]!=oc.id&&allowedColors.includes(_card.color(oc))&&statCmp(oc)
-    ).map(oc=>{return{color:_card.color(oc),id:oc[0],level:oc[1],name:_card.name(oc)}})
+    ).map(oc=>({color:_card.color(oc),id:oc[0],level:oc[1],name:_card.name(oc)}))
     if(better.length)return[c[0],better]
   }).filter(x=>x)
 )
@@ -201,41 +231,45 @@ const sortMyCards=cardscores=>{
  * @returns {Array} array of playable teams
  */
 const basicCards=Object.fromEntries(_card.basic.map(c=>[c,1]));
-const playableTeams = (battles,{mana_cap,ruleset,inactive,quest,oppCards={},myCards=basicCards,sortByWinRate}/*,fn='lastMatch'*/) => {
-  //ruleset matching could be improved
-  //Object.assign(oppCards,basicCards);
-  const res2Score = {w:1,d:-0.5,l:-1.27},xer = {r:1.3,s:6};
+_score.xtrctRules=ruleset=>{
   let {attr_r, card_r} = ruleset.split('|').reduce((rules,cr)=>{
     _team.rules.secondary.includes(cr)?(rules.card_r=cr):rules.attr_r.push(cr);
     return rules},{attr_r:[]})
-  attr_r[0]??='Standard'
+  attr_r[0]??='Standard';attr_r.sort();
+  return {attr_r,card_r}
+}
+const playableTeams = (battles,{mana_cap,ruleset,inactive,quest,oppCards={},myCards=basicCards,sortByWinRate,wBetterCards}) => {
+  //ruleset matching could be improved
+  //Object.assign(oppCards,basicCards);
+  const {attr_r,card_r}=_score.xtrctRules(ruleset);
+  const res2Score = {w:1,d:-0.81,l:-1.27},xer = {r:1.27,s:4};
   _dbug.table({RuleSet:{ruleset,card_r,attr_r}})
   var filteredTeams=[],cardscores={},battlesList = battles;
   for(let path of attr_r)battlesList=battlesList[path];//This assumes object exists
-  const table = new _dbug.tt();
   for(let mana of Object.keys(battlesList).filter(x=>x<=mana_cap&&Number(x)).sort((a,b)=>b-a)){
-    res2Score.l*=mana_cap/mana;res2Score.w*=mana/mana_cap;xer.r*=mana/mana_cap;xer.s*=mana/mana_cap;
+    res2Score.l*=mana_cap/mana;res2Score.w*=mana/mana_cap;res2Score.d*=mana_cap/mana;
+    xer.r*=mana/mana_cap;xer.s*=mana/mana_cap;
     const scores = teamScores(battlesList[mana]
       ,{res2Score,xer,mana_cap,inactive,cardscores,myCards,oppCards});
-    table.e = {/*[`#battles - ${mana}`]:battlesList[mana].length,*/'#Scores':scores.size,
+    _dbug.tt.score = {'#Scores':scores.size,
       '#teams':filteredTeams.push(
         ...[...scores.entries()].filter(([t,s])=>
           t.length>2    && _team.isActive(t,inactive) &&
           (s.count<2*s._w || s.count<2*s.w) && _arr.chunk2(t).every(c=>myCards[c[0]]>=c[1])
-          && filterTeamByRules(_arr.chunk2(t),card_r)
+          && _score.filterTeamByRules(_arr.chunk2(t),card_r)
         ).sort(sortByProperty(sortByWinRate)).filter((_,i,{length})=>i<length/3)
-        .map(([t,s])=>{return {team:_arr.chunk2(t),...s}})
+        .map(([t,s])=>({team:_arr.chunk2(t),...s}))
       ),...res2Score,...xer}
     if(sortByWinRate||(filteredTeams.length>27))break;
-  }table.done;
+  }delete _dbug.tt.score;
   var filteredTeams_length = filteredTeams.length;
-  filteredTeams.sort(sortByProperty(sortByWinRate)).splice(3+filteredTeams.length/27)
+  filteredTeams.sort(sortByProperty(sortByWinRate)).map((x,i)=>x.rank=i).splice(3+filteredTeams.length/27)
   log('trimming', {filteredTeams_length},'to',filteredTeams.length)
   if(quest)priorByQuest(filteredTeams,quest);
   //writeFile(`data/${player}_${fn}.json`, filteredTeams).catch(log);
-  const mycards = Object.entries(myCards).filter(c=>!inactive.includes(_card.color(c))&&cardPassRules(card_r)(c))
+  const mycards = Object.entries(myCards).filter(c=>!inactive.includes(_card.color(c))&&_score.cardRules(card_r)(c))
     .map(c=>[Number(c[0]),c[1],Number(cardscores[c[0]]?.pos)])
     .sort(sortMyCards(cardscores))
-  return filteredTeams.map(teamWithBetterCards(betterCards(mycards,ruleset),mycards,{mana_cap,sortByWinRate,ruleset}));
+  return filteredTeams.map(_score.wBetterCards(_score.bCards(mycards,ruleset),mycards,{wBetterCards,mana_cap,sortByWinRate,ruleset}));
 }
-module.exports = {teamWithBetterCards,teamScores,filterTeamByRules,playableTeams,scoreXer,betterCards};
+module.exports = {_score,teamScores,playableTeams};
